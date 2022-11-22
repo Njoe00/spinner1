b3743b74a37327535dd811a5eab09095a9353780
Njoe00 <nick.james.joe@gmail.com>
2022-11-22 23:00:36 +0000

    Refactored spinner to have less code

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+const gif = ['\r|  ', '\r/  ', '\r- ', '\r\\ ', '\r|  '];
+
+const spinner = (gif) => {
+    let timer = 100;
+    for (let spot of gif) {
+        setTimeout(() => {
+          process.stdout.write(spot);
+        }, timer);
+        timer += 200;
+      }
+};
+spinner(gif)
+
